c48e9733b240ec5c1c1a2871339fa343e0407645
Mateusz Cogiel <jank7947@gmail.com>
2023-01-15 14:02:54 +0100

    Create necessary entities.
    Create all necessary entities in SchoolRegiter/Entities.

---
+++ SchoolRegister.Entities/GradeEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolRegister.Entities;
+
+public class GradeEntity
+{
+    public int Id { get; set; }
+
+    public DateTime DateOfIssue { get; set; }
+
+    public virtual GradeValueEntity GradeValue { get; set; }
+
+    public Guid StudentId { get; set; }
+
+    public virtual StudentEntity Student { get; set; }
+
+    public int SubjectId { get; set; }
+
+    public virtual Subject Subject { get; set; }
+}
---
+++ SchoolRegister.Entities/GradeValueEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Entities;
+
+public enum GradeValueEntity
+{
+    A_Excellent,
+    B_VeryGood,
+    C_Good,
+    D_Satisfactory,
+    E_Sufficient
+}
+
