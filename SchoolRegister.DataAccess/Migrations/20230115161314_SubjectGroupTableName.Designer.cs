d4100c32cff5b4f49e76de884c48a21f713cc8c1
Mateusz Cogiel <jank7947@gmail.com>
2023-01-15 17:22:25 +0100

    Data seed and name set for SubjectGroup table.
    Data seed with default values and name set for SubjectGroup table.

--- SchoolRegister.DataAccess/Migrations/20230115161314_SubjectGroupTableName.Designer.cs
+++ SchoolRegister.DataAccess/Migrations/20230115161314_SubjectGroupTableName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolRegister.DataAccess;
 
@@ -11,9 +12,11 @@ using SchoolRegister.DataAccess;
 namespace SchoolRegister.DataAccess.Migrations
 {
     [DbContext(typeof(SchoolRegisterContext))]
-    partial class SchoolRegisterContextModelSnapshot : ModelSnapshot
+    [Migration("20230115161314_SubjectGroupTableName")]
+    partial class SubjectGroupTableName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
