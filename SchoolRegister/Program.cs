feeb005c0a7c6e3992b284b62f7015dd59bfcb37
Mateusz Cogiel <jank7947@gmail.com>
2023-01-18 02:17:50 +0100

    Program class cleanup.
    Created extension methods for Program.cs to make it more readable.

--- SchoolRegister/Program.cs
+++ SchoolRegister/Program.cs
@@ -1,114 +1,26 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using SchoolRegister.BusinessAccess.Interfaces;
-using SchoolRegister.BusinessAccess.Services;
-using SchoolRegister.DataAcces.Repository;
-using SchoolRegister.DataAcces.Repository.IRepository;
-using SchoolRegister.DataAccess;
-using SchoolRegister.Entities;
-using SchoolRegister.Models.Models;
-using SchoolRegister.Utility.SeedData;
-using SchoolSystem.Utility.SeedData;
-using Swashbuckle.AspNetCore.Filters;
-using System.Text;
+using SchoolRegister.WebAPI.ProgramExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Add DbContext.
-builder.Services.AddDbContext<SchoolRegisterContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.RegisterDatabase(builder);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 
-#region Swagger Configuration
-// Swagger Authentication Configuration.
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
+builder.Services.RegisterSwagger();
 
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-});
-#endregion
+builder.Services.RegisterServices();
 
-// Register UnitOfWork for repositories.
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.RegisterCors();
 
-// Register Services.
-builder.Services.AddScoped<IGradeService, GradeService>();
-builder.Services.AddScoped<IGroupService, GroupService>();
-builder.Services.AddScoped<ISubjectService, SubjectService>();
-builder.Services.AddScoped<IUserService, UserService>();
-
-builder.Services.AddScoped<IPasswordHasher<UserEntity>, PasswordHasher<UserEntity>>();
-
-#region CORS
-// CORS Configuration.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("frontEndClient", builder =>
-    builder.AllowAnyMethod() // Allow any Method.
-    .AllowAnyHeader() // Allow any Header.
-    .WithOrigins("http://localhost:8080")); // Source for which we allow the CORS policy. 
-});
-#endregion
-
-#region Authentication
-// Authentication configuration
-var authenticationSettings = new AuthenticationSettings();
-
-builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
-
-builder.Services.AddSingleton(authenticationSettings);
-
-builder.Services.AddAuthentication(option =>
-{
-    option.DefaultAuthenticateScheme = "Bearer";
-    option.DefaultScheme = "Bearer";
-    option.DefaultChallengeScheme = "Bearer";
-}).AddJwtBearer(cfg =>
-{
-    cfg.RequireHttpsMetadata = false; // We do not force only the https protocol from the client.
-    cfg.SaveToken = true; // The given token should be saved on the server side for authentication.
-    // Validation parameters to check if the parameters sent by the client match what the server knows.
-    cfg.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidIssuer = authenticationSettings.JwtIssuer, // Token Issuer.
-        ValidAudience = authenticationSettings.JwtIssuer, // Which entities can use this token (same value because we generate the token within our application).
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)), // Private key generated from JwtKey value.
-    };
-});
-#endregion
+builder.Services.RegisterAuthentication(builder);
 
 var app = builder.Build();
 
-#region SeedData
-using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetService<SchoolRegisterContext>();
-SeedGroups.GroupsSeed(dbContext);
-SeedUsers.RolesSeed(dbContext);
-SeedUsers.UsersSeed(dbContext);
-SeedSubjects.SubjectsSeed(dbContext);
-SeedGrades.GradesSeed(dbContext);
-#endregion
+app.SeedDefaultData();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.ConfigureSwagger();
 
 app.UseCors("frontEndClient");
 
---
+++ SchoolRegister/ProgramExtensions/DependencyInjectionSetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolRegister.BusinessAccess.Interfaces;
+using SchoolRegister.BusinessAccess.Services;
+using SchoolRegister.DataAcces.Repository.IRepository;
+using SchoolRegister.DataAcces.Repository;
+using SchoolRegister.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace SchoolRegister.WebAPI.ProgramExtensions;
+
+public static class DependencyInjectionSetup
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        // Register UnitOfWork for repositories.
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        // Register Services.
+        services.AddScoped<IGradeService, GradeService>();
+        services.AddScoped<IGroupService, GroupService>();
+        services.AddScoped<ISubjectService, SubjectService>();
+        services.AddScoped<IUserService, UserService>();
+
+        // Register PasswordHasher.
+        services.AddScoped<IPasswordHasher<UserEntity>, PasswordHasher<UserEntity>>();
+
+        return services;
+    }
+}
